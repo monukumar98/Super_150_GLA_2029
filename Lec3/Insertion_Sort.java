6c97b3046a30fc0928954b44094617f1f63ee6ea
monukumar98 <monu012as@gmail.com>
2026-08-20 16:58:04 +0530

    Sorting Kadanes Moore voting

---
+++ Lec3/Insertion_Sort.java
@@ -0,0 +1,31 @@
+package Lec3;
+
+public class Insertion_Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 8, 9, 1, 7, 4, 2, 11 };
+		Sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public static void Sort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			InsertLastElement(arr, i);
+		}
+
+	}
+
+	public static void InsertLastElement(int[] arr, int i) {
+		int j = i - 1;
+		int item = arr[i];
+		while (j >= 0 && arr[j] > item) {
+			arr[j + 1] = arr[j];
+			arr[j] = item;
+			j--;
+		}
+		// correct posstions is j+1
+	}
+}
---
+++ Lec3/Selection_Sort.java
@@ -0,0 +1,34 @@
+package Lec3;
+
+public class Selection_Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 4, -1, 5, 3, 2, 1, 7 };
+		// System.out.println(min_form_ith_index(arr, 2));
+		Sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public static void Sort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int mini = min_form_ith_index(arr, i);
+			int temp = arr[i];
+			arr[i] = arr[mini];
+			arr[mini] = temp;
+		}
+	}
+
+	public static int min_form_ith_index(int[] arr, int idx) {
+		int mini = idx;
+		for (int i = idx + 1; i < arr.length; i++) {
+			if (arr[i] < arr[mini]) {
+				mini = i;
+			}
+		}
+		return mini;
+
+	}
+}
